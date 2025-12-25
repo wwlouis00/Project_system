85268e738510c08c52bd22c2077f7bb1dc4f637d
wwlouis00 <wwlouis00@gmail.com>
2025-12-25 22:51:26 +0800

    update src/autostart.sh

--- src/autostart.sh
+++ src/autostart.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+DIR="$HOME/eggi_release/socket_cam"
 
-cd /home/pi/eggi_release/socket_cam
+if [ ! -d "$DIR" ]; then
+    mkdir -p "$DIR"
+    echo "[$(date)] Created $DIR"
+fi
+
+cd "$DIR" && python3 -u main.pyc >> main.txt 2>&1
 python3 -u main.pyc > main.txt
